1b2de205b9045ae5953605ad28ea4531733c88ef
iCAROS7 <hominlab@gmail.com>
2021-06-11 04:57:48 +0900

    DataAnalytics 클래스 추가

---
+++ IsItRight/DataAnalytics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace IsItRight
+{
+    public class DataAnalytics
+    {
+        private SeoulOpenData so;
+        
+        public DataAnalytics(SeoulOpenData seoulOpenData)
+        {
+            Debug.WriteLine(@"INFO: New DataAnalytics initalizing: " + nameof(seoulOpenData));
+            so = seoulOpenData;
+        }
+
+        /// <summary>
+        /// 합의 평균을 구합니다.
+        /// </summary>
+        /// <param name="value">취합할 데이터 name</param>
+        /// <param name="row">0-4, 취합할 데이터 row 수</param>
+        public double GetSumArg(string value)
+        {
+            Debug.WriteLine(@"INFO: Call GetSumArg");
+            int row = (so.Time == @"") ? 0 : (23 - Int32.Parse(so.Time) > 4) ? Int32.Parse(so.Time) : 4;
+            Debug.WriteLine(@"INFO: Set Row: " + row);
+            
+            double sum = 0;
+            
+            // TODO: 결과값 확인 요망 
+            for (int i = 0; i <= row; i++)
+            {
+                if (Double.TryParse(so.GetValue(value, i), out double temp))
+                {
+                    Debug.WriteLine(temp);
+                    sum += temp;
+                    Debug.WriteLine(sum);
+                }
+                else
+                {
+                    Debug.WriteLine(@"Is Here?");
+                    return sum / row;
+                }
+            }
+            
+            return sum / row;
+        }
+    }
+}
